ee1ea8268228aaca7efe971cd0dff79f2a972b9e
Chris Dent <chris.dent@gmail.com>
2010-06-09 20:21:43 +0100

    Improve routing.
    Use regex to do the match.

--- app.js
+++ app.js
@@ -2,42 +2,82 @@ var sys = require('sys'),
     http = require('http'),
     url = require('url');
 
-application = {
+var front_page = "<html> <head> <title>Welcome</title> </head> <body> <ul> <li><a href='/bags'>bags</a></li> <li><a href='/recipes'>recipes</a></li> </ul> </body> </html> ";
+
+var application = {
     routes: {
-                '/': {
+                '\/': {
                     GET: function(req, res) {
-                        res.writeHead(200, {'Content-Type': 'text/plain'});
-                        res.end('/\n');
+                        res.writeHead(200, {'Content-Type': 'text/html'});
+                        res.end(front_page);
                      },
                 },
-                '/bags': {
+                '\/bags\/?': {
                     GET: function(req, res) {
                         res.writeHead(200, {'Content-Type': 'text/plain'});
                         res.end('/bags\n');
                      },
                 },
-                '/recipes': {
+                '\/recipes\/?': {
                     GET: function(req, res) {
                         res.writeHead(200, {'Content-Type': 'text/plain'});
                         res.end('/recipes\n');
                      },
                 },
-                '/recipes/foobar': {
+                '\/recipes\/(\\w+)\/?': {
                     GET: function(req, res) {
                         res.writeHead(200, {'Content-Type': 'text/plain'});
-                        res.end('/recipes\n');
+                        res.end('/recipes/' + RegExp.$1 + '\n');
+                     },
+                },
+                '\/recipes\/(\\w+)\/tiddlers\/?': {
+                    GET: function(req, res) {
+                        res.writeHead(200, {'Content-Type': 'text/plain'});
+                        res.end('/recipes/' + RegExp.$1 + '/tiddlers' + '\n');
+                     },
+                },
+                '\/recipes\/(\\w+)\/tiddlers\/(\\w+)\/?': {
+                    GET: function(req, res) {
+                        res.writeHead(200, {'Content-Type': 'text/plain'});
+                        res.end('/recipes/' + RegExp.$1 + '/tiddlers/' + RegExp.$2+ '\n');
+                     },
+                },
+                '\/bags\/(\\w+)\/?': {
+                    GET: function(req, res) {
+                        res.writeHead(200, {'Content-Type': 'text/plain'});
+                        res.end('/bags/' + RegExp.$1 + '\n');
+                     },
+                },
+                '\/bags\/(\\w+)\/tiddlers\/?': {
+                    GET: function(req, res) {
+                        res.writeHead(200, {'Content-Type': 'text/plain'});
+                        res.end('/bags/' + RegExp.$1 + '/tiddlers' + '\n');
+                     },
+                },
+                '\/bags\/(\\w+)\/tiddlers\/(\\w+)\/?': {
+                    GET: function(req, res) {
+                        res.writeHead(200, {'Content-Type': 'text/plain'});
+                        res.end('/bags/' + RegExp.$1 + '/tiddlers/' + RegExp.$2+ '\n');
                      },
                 },
             },
 };
 
+// configure and start the server
 http.createServer(function (req, res) {
         var parsed_url = url.parse(req.url, true);
         var path = parsed_url.pathname;
         var method = req.method;
 
-        route = application.routes[path];
-
+        // find a matching route
+        var route = undefined;
+        for (pattern in application.routes) {
+            if (path.match('^' + pattern + '$')) {
+                route = application.routes[pattern];
+                break;
+            }
+        }
+        
         if (route == undefined) {
             res.writeHead(404, {'Content-Type': 'text/plain'});
             res.end(path + ' not found' + '\n');
@@ -51,4 +91,5 @@ http.createServer(function (req, res) {
             }
         }
     }).listen(8000);
+
 sys.puts('Server running at http://127.0.0.1:8000/');
